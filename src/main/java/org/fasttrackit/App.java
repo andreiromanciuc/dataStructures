6ac13f7121ceb258e3a2d5f1df590ee1f7ea1a29
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-01-30 10:55:56 +0200

    Chapter 7 - Ex. 1 (ex.33)

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,11 @@
+package org.fasttrackit;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Magic8Ball magic8Ball = new Magic8Ball();
+        magic8Ball.getAnswers();
+
+    }
+}
